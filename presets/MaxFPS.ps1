07d9e3429f9811c29b8943ea8bd05ea2f8b5c84c
Verzidereal <minquizreal@gmail.com>
2026-01-30 12:39:00 -0600

    Add MaxFPS preset optimizations

--- presets/MaxFPS.ps1
+++ presets/MaxFPS.ps1
@@ -1,1 +1,12 @@
+Write-Host "[Preset] Applying MaxFPS preset..."
 
+Disable-Cortana
+Disable-Bloatware
+Optimize-GPU
+Optimize-Scheduler
+Optimize-TCP
+Disable-Nagle
+Optimize-NetworkAdapter
+Enable-UltimatePerformance
+
+Write-Host "[MaxFPS] Done!"
