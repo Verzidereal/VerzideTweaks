c04c2b1d8c1bd88d0e86159d29e746c50ae183cc
Verzidereal <minquizreal@gmail.com>
2026-01-30 12:41:20 -0600

    Delete utils.ps1

--- utils.ps1
+++
@@ -1,5 +0,0 @@
-function Write-Info ($msg) { Write-Host "[*] $msg" -ForegroundColor Cyan }
-function Write-Success ($msg) { Write-Host "[+] $msg" -ForegroundColor Green }
-function Write-WarningMsg ($msg) { Write-Host "[!] $msg" -ForegroundColor Yellow }
-function Write-ErrorMsg ($msg) { Write-Host "[X] $msg" -ForegroundColor Red }
-
