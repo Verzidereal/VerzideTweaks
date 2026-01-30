5e793310513b38b481e47b9df4066a69354418b0
Verzidereal <minquizreal@gmail.com>
2026-01-30 12:34:41 -0600

    Add security functions for privacy and malware removal

--- modules/Security/Security.psm1
+++ modules/Security/Security.psm1
@@ -1,1 +1,38 @@
+function Apply-PrivacyTweaks {
+    Write-Host "[+] Applying privacy-safe tweaks..."
+
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo" /v Enabled /t REG_DWORD /d 0 /f
+    reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" /v AllowTelemetry /t REG_DWORD /d 0 /f
+}
+
+function Remove-MalwareApps {
+    Write-Host "[+] Removing known malicious OEM apps..."
+
+    $junk = @(
+        "WildTangent",
+        "McAfee",
+        "Norton",
+        "CandyCrush",
+        "Booking",
+        "HPJumpStart"
+    )
+
+    foreach ($app in $junk) {
+        Get-AppxPackage *$app* -AllUsers | Remove-AppxPackage -ErrorAction SilentlyContinue
+        Write-Host " ✓ Removed: $app"
+    }
+}
+
+function Apply-FirewallGamingPreset {
+    Write-Host "[+] Applying firewall gaming preset..."
+
+    netsh advfirewall set allprofiles state on
+    netsh advfirewall firewall add rule name="Gaming UDP Allow" dir=in action=allow protocol=UDP localport=1024-65535
+}
+
+function Enable-WindowsDefender {
+    Write-Host "[+] Ensuring Windows Defender is enabled..."
+
+    Set-MpPreference -DisableRealtimeMonitoring $false
+}
 
