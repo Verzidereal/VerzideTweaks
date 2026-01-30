55537e688eaeb98fd846cd2f510b8694df4de0ef
Verzidereal <minquizreal@gmail.com>
2026-01-30 12:39:14 -0600

    Add LowLatency preset script for performance optimization

--- presets/LowLatency.ps1
+++ presets/LowLatency.ps1
@@ -1,1 +1,11 @@
+Write-Host "[Preset] Applying LowLatency preset..."
+
+Set-HPET
+Optimize-TCP
+Disable-Nagle
+Optimize-NetworkAdapter
+Reduce-InputLag
+Apply-FirewallGamingPreset
+
+Write-Host "[LowLatency] Done!"
 
