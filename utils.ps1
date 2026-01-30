4fe4b14a2391073d812e26547d7bd52a3d71e095
Verzidereal <minquizreal@gmail.com>
2026-01-30 12:10:28 -0600

    Add logging functions for info, success, warning, and error

--- utils.ps1
+++ utils.ps1
@@ -1,1 +1,5 @@
+function Write-Info ($msg) { Write-Host "[*] $msg" -ForegroundColor Cyan }
+function Write-Success ($msg) { Write-Host "[+] $msg" -ForegroundColor Green }
+function Write-WarningMsg ($msg) { Write-Host "[!] $msg" -ForegroundColor Yellow }
+function Write-ErrorMsg ($msg) { Write-Host "[X] $msg" -ForegroundColor Red }
 
